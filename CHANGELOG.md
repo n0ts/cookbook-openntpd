ebe7af4bc229dc5d8c62e49bed9c2cd906838508
Andreas Lappe <nd@off-pist.de>
2013-04-04 13:33:17 +0200

    Initial commit

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+# CHANGELOG for openntpd
+
+This file is used to list changes made in each version of openntpd.
+
+## 0.1.0:
+
+* Initial release of openntpd
---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'openntpd'
+maintainer       'kaeufli.ch'
+maintainer_email 'nd@kaeufli.ch'
+license          'MIT'
+description      'Installs/Configures openntpd'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
